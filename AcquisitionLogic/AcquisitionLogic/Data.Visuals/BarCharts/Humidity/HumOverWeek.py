e4eeff5e02db1381fa73be181c78b342f3fd7db0
Raj Mody <rajtfib@hotmail.ca>
2016-10-28 00:52:38 -0400

    RD - AqL - Folder Structure Modification and added random plot designer for Humidity to test matplotlib installation.

---
+++ AcquisitionLogic/AcquisitionLogic/Data.Visuals/BarCharts/Humidity/HumOverWeek.py
@@ -0,0 +1,37 @@
+import numpy as npy
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from random import randint as rand
+
+week = 7
+weekDataList = list()
+# demo data
+for x in range(0,7):
+    hum = rand(0,100)
+    weekDataList.append(hum)
+
+index = npy.arange(week)
+width = 0.40
+
+fig, ax = plt.subplots()
+barSet1 = ax.bar(index, weekDataList, width, color='r')
+
+ax.set_ylabel('Humidity (%)')
+ax.set_title('Humidity Data Captured Over Current Week')
+ax.set_xticks(index + width)
+ax.set_xticklabels(('Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'))
+
+ax.legend(barSet1, ('DHT11 - Main'))
+
+def autolabel(bars):
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width(),
+                1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+autolabel(barSet1)
+
+plt.show()
