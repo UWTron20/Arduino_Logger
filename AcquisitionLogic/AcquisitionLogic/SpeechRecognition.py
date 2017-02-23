6b5b1ae7923d07ea6ce3051c42d78998ec71455f
Raj Mody <rajtfib@hotmail.ca>
2017-02-22 21:44:49 -0500

    Speech Recognition Test - see import

---
+++ AcquisitionLogic/AcquisitionLogic/SpeechRecognition.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+
+class SpeechRecognition(object):
+    """Speech through input"""
+    def __init__(self):
+        self.RecentCommandAudio = None
+        self.r = sr.Recognizer()
+
+    # obtain audio from the microphone    
+    def CommandCapture(self):
+        with sr.Microphone() as source:
+            print("Say something!")
+            self.RecentCommandAudio = self.r.listen(source)
+    def ReadBackLastCommand(self):
+        if self.RecentCommandAudio != None:        
+            try:
+                print("Sphinx thinks you said " + self.r.recognize_sphinx(self.RecentCommandAudio))
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e)) 
+        else:
+            print("No Command Was Said")
+
+while True:
+    Sp = SpeechRecognition()
+    Sp.CommandCapture()
+    Sp.ReadBackLastCommand()
+
+
